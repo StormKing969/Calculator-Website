7600bcb1ad9ad2b40acf5d1f35104bb67ef289ea
StormKing969 <wijesinghesaj@gmail.com>
2021-10-04 22:01:51 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var clickCounter = 0;
 var display = [];
 var memory = [];
+var operator = [];
 
 
 document.querySelector(".background_changer").addEventListener("change", function() {
@@ -29,10 +30,92 @@ function updateScreen(screenInput) {
 }
 
 function Calculator(userInput) {
-    console.log(userInput);
 
-    if (userInput == "+" || userInput == "-" || userInput == "x" || userInput == "/") {
-        display = [0];
-        updateScreen(display);
+    if (userInput != "+" & userInput != "-" &  userInput != "x" & userInput != "/") {
+        display += [userInput];
+        updateScreen(display);  
+    }
+
+    switch (userInput) {
+        case "DEL":
+            var temp = display.slice(0, display.length - 4);
+            display = temp;
+            updateScreen(display); 
+            break
+
+        case "RESET":
+            display = [];
+            updateScreen(display); 
+            break
+
+        case "+":
+            var temp = display.slice(0, display.length - 0);
+            memory = temp;
+            display = [];
+            updateScreen(display); 
+            operator = "+";
+
+            // if(operator != null && operator != '') {
+            //     var temp = display.slice(0, display.length - 1);
+            //     display = [];
+            //     var operationOutput = Operation(memory, operator, temp)
+            //     updateScreen(operationOutput); 
+            //     display = [operationOutput];
+            // }
+            break
+        
+        case "-":
+            var temp = display.slice(0, display.length - 0);
+            memory = temp;
+            display = [];
+            updateScreen(display); 
+            operator = "-";
+            break
+
+        case "x":
+            var temp = display.slice(0, display.length - 0);
+            memory = temp;
+            display = [];
+            updateScreen(display); 
+            operator = "x";
+            break
+
+        case "/":
+            var temp = display.slice(0, display.length - 0);
+            memory = temp;
+            display = [];
+            updateScreen(display); 
+            operator = "/";
+            break
+
+        case "=":
+            var temp = display.slice(0, display.length - 1);
+            display = [];
+            var operationOutput = Operation(memory, operator, temp);
+            updateScreen(operationOutput); 
+            display = [operationOutput];
+            operator = [];
+    }
+}
+
+function Operation(firstValue, operatorType, secondValue) {
+    if (operatorType == "+") {
+        var answer = parseFloat(firstValue) + parseFloat(secondValue);
+        return answer;
+    }
+
+    if (operatorType == "-") {
+        var answer = parseFloat(firstValue) - parseFloat(secondValue);
+        return answer;
+    }
+
+    if (operatorType == "x") {
+        var answer = parseFloat(firstValue) * parseFloat(secondValue);
+        return answer;
+    }
+
+    if (operatorType == "/") {
+        var answer = parseFloat(firstValue) / parseFloat(secondValue);
+        return answer;
     }
 }
